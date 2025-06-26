2ecf7182b71ada67604c82d128f1214359f6e6b6
Sneha <snehaknayak07@gmail.com>
2025-06-26 19:41:05 +0530

    Add files via upload

--- Problem-1.py
+++ Problem-1.py
@@ -1,1 +1,20 @@
+class Calculator:
+    def __init__(self, a: float, b: float, operation: str):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower()
 
+    def calculate(self):
+        if self.operation == 'add':
+            return self.a + self.b
+        elif self.operation == 'subtract':
+            return self.a - self.b
+        elif self.operation == 'multiply':
+            return self.a * self.b
+        elif self.operation == 'divide':
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Division by zero is not allowed."
+        else:
+            return "Invalid operation"
